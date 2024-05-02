6f6f51c13e14fa75286a0864a309db20ed302ad1
Lena1292 <elena.krylataya@gmail.com>
2024-05-02 20:55:36 +0300

    add classes

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,11 +1,24 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase {
 
+
     @Test
-    public void test(){
+    public void loginSuccess(){
+       app.getHelperUser().openLoginRegistrationForm();
+       app.getHelperUser().fillLoginRegistrationForm("elena.krylataya@gmail.com", "Lenakira11$");
+       app.getHelperUser().submitLogin();
+
+      //  Assert
+//        Assert.assertEquals();
+//        Assert.assertNotEquals();
+//        Assert.assertTrue();
+//        Assert.assertFalse();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
 
 
     }
