f8d419112b1a4456cdcb4b9bf3d848b6b80a4d6f
Lena1292 <elena.krylataya@gmail.com>
2024-06-02 21:28:51 +0300

    add listener

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -16,7 +16,10 @@ public class HelperUser extends HelperBase {
 //    WebElement loginTab = wd.findElement(By.cssSelector("a[href='/login']"));
 //    loginTab.click();
         click(By.cssSelector("a[href='/login']"));
+        logger.info("open form by click on button with locator By.cssSelector(\"a[href='/login']\") ");
     }
+
+
 
     public void fillLoginRegistrationForm(String email, String password) {
 //        WebElement emailInput = wd.findElement(By.name("email"));
@@ -24,6 +27,7 @@ public class HelperUser extends HelperBase {
 //        emailInput.clear();
 //        emailInput.sendKeys(email);
         type(By.name("email"), email);
+        logger.info("type in input with locator By.name(\"email\")");
 
 //        WebElement passwordInput = wd.findElement(By.xpath("//input[last()]"));
 //        passwordInput.click();
@@ -36,6 +40,7 @@ public class HelperUser extends HelperBase {
     public void fillLoginRegistrationForm(User user) {
         type(By.name("email"), user.getEmail());
         type(By.xpath("//input[last()]"), user.getPassword());
+
     }
 
     public void submitLogin() {
